ccae891cb3c7bf7080b9222451e2f76d59cdd7d1
Erick Romero <erickrom@gmail.com>
2013-12-09 21:31:08 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Merlin::Application.config.secret_key_base = '033c74645d2def7b65531cec7bec50dd68222f65891590c4d00a6aec9a0ef88f9213d02f76ce27608dfa11dcf1a26b9cc05c671a1f98ba541bba673574e1039a'
