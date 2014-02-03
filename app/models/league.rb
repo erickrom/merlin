150e50be275e3254542334c24263cce3d72348c3
Erick Romero <erickrom@gmail.com>
2014-02-02 22:49:50 -0800

    Adds uniqueness validation to a League's external_id and ensures we only update the league's attributes we care about.

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,4 +1,5 @@
 class League < ActiveRecord::Base
+  validates_uniqueness_of :external_id
 
   class << self
     def save_leagues_from_json(json_payload)
@@ -16,9 +17,9 @@ class League < ActiveRecord::Base
     private
 
     def create_from_json(json_league)
-      League.new(
-        name: json_league["name"],
-        external_id: json_league["id"].to_i,
+      league = League.where(external_id: json_league["id"].to_i).first_or_initialize
+      league.name = json_league["name"] unless league.name.present?
+      league.update_attributes(
         year: json_league["year"].to_i,
         group_code: json_league["group_code"].to_i,
         playoff: json_league["playoff"] == "1",
@@ -27,7 +28,7 @@ class League < ActiveRecord::Base
         total_rounds: json_league["total_rounds"].to_i,
         flag_url_path: json_league["flag"]
       )
+      league
     end
-
   end
 end
--- spec/models/league_spec.rb
+++ spec/models/league_spec.rb
@@ -11,6 +11,14 @@ describe League do
   it { should respond_to(:total_rounds) }
   it { should respond_to(:flag_url_path) }
 
+  context ".external_id" do
+    it "should be unique" do
+      league = create(:league)
+      duplicate_league = League.new(name: 'Another League', external_id: league.external_id)
+      duplicate_league.should_not be_valid
+    end
+  end
+
   describe ".save_from_json_and_return_all" do
     let(:json) { load_fixture('leagues_response_top', 'json') }
 
@@ -32,6 +40,37 @@ describe League do
       expect(league.total_group).to eq(8)
       expect(league.total_rounds).to eq(6)
       expect(league.flag_url_path).to eq('/media/img/flags/44x27/ue.png')
+    end
+
+    context "when called again" do
+      it "updates the attributes we care about" do
+        subject
+        league = League.first
+        expect(league.name).to eq("Champions League")
+        expect(league.external_id).to eq(107)
+        expect(league.year).to eq(2014)
+        expect(league.group_code).to eq(1)
+        expect(league.playoff).to be_false
+        expect(league.current_round).to eq(6)
+        expect(league.total_group).to eq(8)
+        expect(league.total_rounds).to eq(6)
+        expect(league.flag_url_path).to eq('/media/img/flags/44x27/ue.png')
+
+        league.update_attributes!(name: 'La Champions League')
+
+        League.save_leagues_from_json(json)
+
+        expect(league.reload.name).to eq('La Champions League')
+        expect(league.external_id).to eq(107)
+        expect(league.year).to eq(2014)
+        expect(league.group_code).to eq(1)
+        expect(league.playoff).to be_false
+        expect(league.current_round).to eq(6)
+        expect(league.total_group).to eq(8)
+        expect(league.total_rounds).to eq(6)
+        expect(league.flag_url_path).to eq('/media/img/flags/44x27/ue.png')
+
+      end
     end
   end
 end
